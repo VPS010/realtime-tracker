a9678911acdf5d2d8877ad9c36398432c11938ae
Vinay Prakash Senapati <vinayprakashsenapati01@gmail.com>
2024-09-08 16:41:51 +0530

    optimised

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-const http = require("http");
-const socketio = require("socket.io");
+const http = require('http');
+const socketio = require('socket.io');
 
 const server = http.createServer(app);
-
 const io = socketio(server);
 
-io.on("connection", function(socket) {
-   socket.on("send-location", function(data) {
-      io.emit("receive-location", { id: socket.id, ...data });
-   });
-   console.log("connected");
-
-   socket.on("disconnect", function() {
-      io.emit("user-disconnected", socket.id);
-   });
-});
-
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Set views directory
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
-   res.render("index");
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('send-location', (data) => {
+        io.emit('receive-location', { id: socket.id, ...data });
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('user-disconnected', socket.id);
+        console.log('Client disconnected');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
 server.listen(3000, () => {
-   console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,60 @@
 const socket = io();
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position)=>{
+socket.on('connect', () => {
+    console.log('Connected to server');
+});
 
-        const{latitude,longitude}=position.coords;
-        socket.emit("send-location",{latitude,longitude});
-        console.log("reloded");
-    },(error)=>{
-        console.log(error);
-    },{
-        enableHighAccuracy: true,
-        timeout: 3000,
-        maximumAge:0
-        });
+socket.on('connect_error', (error) => {
+    console.error('Connection Error:', error);
+});
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit('send-location', { latitude, longitude });
+            console.log('Location sent');
+        },
+        (error) => {
+            console.error('Geolocation Error:', error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 3000,
+            maximumAge: 0,
+        }
+    );
+} else {
+    console.error('Geolocation is not supported by this browser.');
 }
 
-const map=L.map("map").setView([0,0],20);
+const map = L.map('map').setView([0, 0], 20);
 
- L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution : "OpenStreetMap" }).addTo(map)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'OpenStreetMap',
+}).addTo(map);
 
- const markers={};
+const markers = {};
+let lastUpdate = 0;
 
- socket.on("receive-location",(data)=>{
+socket.on('receive-location', (data) => {
+    const { id, latitude, longitude } = data;
 
-    const {id,latitude,longitude}= data;
-    map.setView([latitude,longitude],20);
-if(markers[id]){
-    markers[id].setLatLng([latitude,longitude]);
-}
-else{
-    markers[id]=L.marker([latitude,longitude]).addTo(map);
-}
+    if (Date.now() - lastUpdate > 1000) { 
+        map.setView([latitude, longitude], 20);
+        lastUpdate = Date.now();
+    }
 
- });
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
 
- socket.on("user-disconnected",(id)=>{
-    if(markers[id]){
+socket.on('user-disconnected', (id) => {
+    if (markers[id]) {
         map.removeLayer(markers[id]);
         delete markers[id];
     }
- })
+});
